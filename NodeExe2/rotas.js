545245ddbe9f468ee0064d3c71f08712c917ee9b
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2022-09-10 12:47:19 -0300

    criando rotas no node e usando express

--- NodeExe2/rotas.js
+++ NodeExe2/rotas.js
@@ -1,2 +1,27 @@
 const express = require('express')
 const rotas = express.Router()
+
+
+const nomesInfo = [
+  {'nome': 'Bob', 'profissão': 'policial'},
+  {'nome': 'Mark', 'profissão': 'padeiro'},
+  {'nome': 'Lucian', 'profissão': 'taxista'},
+  {'nome': 'Legolas', 'profissão': 'alpinista'},
+]
+
+rotas.get('/',(req,res) =>{
+  res.json({ola: 'eae'})
+})
+
+rotas.get('/:nomeid',(req,res) => {
+  const nomes = req.params.nomeid
+  const nomesI = nomesInfo.find(i => i.nome == nomes)
+  if(!nomesI){
+    res.status(404).json({erro:'nome não encontrado', momePesquisado: nomes})
+  }
+  else{
+    res.status(200).json(nomesI)
+  }
+})
+
+module.exports = rotas
